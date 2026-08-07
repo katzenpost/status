18138c85ed98e7e62d0c4c20df847488c122413e
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-07 20:07:21 +0200

    Add the Gyroid (TPMS) view

---
+++ src/katzenpost_status/assets/features/9v-surfaces.js
@@ -0,0 +1,43 @@
+(function () {
+    "use strict";
+    var K = window.KATZEN;
+    if (!K || !window.KATZEN_GEO3D) return;
+    var G = window.KATZEN_GEO3D, PI2var = Math.PI * 2;
+
+    // Parametric surface as a grid mesh; packets route the grid lines.
+    function surf(id, name, pFn, ures, vres, color, camZ) {
+        G.create({ id: id, name: name, rotateSpeed: 0.35, camZ: camZ || 60,
+            layout: function (d, THREE) {
+                var A = [], idx = {}, E = [], i, j;
+                for (i = 0; i <= ures; i++) for (j = 0; j <= vres; j++) { idx[i + '_' + j] = A.length; A.push(pFn(i / ures, j / vres, THREE)); }
+                function add(a, b, c, e) { var k1 = idx[a + '_' + b], k2 = idx[c + '_' + e]; if (k1 != null && k2 != null) E.push({ a: A[k1], b: A[k2], color: color }); }
+                for (i = 0; i < ures; i++) for (j = 0; j < vres; j++) { add(i, j, i + 1, j); add(i, j, i, j + 1); }
+                return G.anchorLayout(d, THREE, A, E);
+            } });
+    }
+    function knn(pts, THREE, k, color) {
+        var E = [], seen = {}, i, j, m;
+        for (i = 0; i < pts.length; i++) {
+            var ds = [];
+            for (j = 0; j < pts.length; j++) if (j !== i) ds.push([pts[i].distanceToSquared(pts[j]), j]);
+            ds.sort(function (a, b) { return a[0] - b[0]; });
+            for (m = 0; m < k && m < ds.length; m++) { var jj = ds[m][1], a = Math.min(i, jj), b = Math.max(i, jj), key = a + '_' + b; if (!seen[key]) { seen[key] = 1; E.push({ a: pts[i], b: pts[jj], color: color }); } }
+        }
+        return E;
+    }
+    // Sample a triply-periodic implicit surface f=0 into a shell point cloud.
+    function tpms(id, name, f, span, eps, sc, color, camZ) {
+        G.create({ id: id, name: name, rotateSpeed: 0.32, camZ: camZ || 58,
+            layout: function (d, THREE) {
+                var N = 30, pts = [], i, j, l;
+                for (i = 0; i <= N && pts.length < 520; i++) for (j = 0; j <= N && pts.length < 520; j++) for (l = 0; l <= N && pts.length < 520; l++) {
+                    var x = -span + 2 * span * i / N, y = -span + 2 * span * j / N, z = -span + 2 * span * l / N;
+                    if (Math.abs(f(x, y, z)) < eps) pts.push(new THREE.Vector3(x * sc, y * sc, z * sc));
+                }
+                if (pts.length < 4) return G.anchorLayout(d, THREE, pts, []);
+                return G.anchorLayout(d, THREE, pts, knn(pts, THREE, 3, color));
+            } });
+    }
+
+    tpms('gyroid', 'Gyroid (TPMS)', function (x, y, z) { return Math.sin(x) * Math.cos(y) + Math.sin(y) * Math.cos(z) + Math.sin(z) * Math.cos(x); }, Math.PI * 1.5, 0.16, 4.0, 0x2ec4b6, 58);
+})();
