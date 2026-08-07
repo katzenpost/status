df08646199226a74154c0d0ce1503445dece8508
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-07 06:13:09 +0200

    Add the Chua attractor view

--- src/katzenpost_status/assets/features/9u-attractors2.js
+++ src/katzenpost_status/assets/features/9u-attractors2.js
@@ -22,4 +22,6 @@
     }
 
     attractor('chen', 'Chen attractor', function (p, dt) { var a = 35, b = 3, c = 28, x = p[0], y = p[1], z = p[2]; return [x + dt * a * (y - x), y + dt * ((c - a) * x - x * z + c * y), z + dt * (x * y - b * z)]; }, [-0.1, 0.5, -0.6], 0.004, 3600, 0.62, [0, 0, 24], 0x2ec4b6, 70);
+
+    attractor('chua', 'Chua attractor', function (p, dt) { var al = 15.6, be = 28, m0 = -1.143, m1 = -0.714, x = p[0], y = p[1], z = p[2]; var f = m1 * x + 0.5 * (m0 - m1) * (Math.abs(x + 1) - Math.abs(x - 1)); return [x + dt * al * (y - x - f), y + dt * (x - y + z), z + dt * (-be * y)]; }, [0.7, 0, 0], 0.02, 4200, 3.4, [0, 0, 0], 0x9b5de5, 60);
 })();
