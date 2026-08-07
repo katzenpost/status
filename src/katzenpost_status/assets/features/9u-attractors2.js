8cfe377e3ac9d0360df73d3a3ae3af362257f8ab
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-07 06:23:42 +0200

    Add the Dadras attractor view

--- src/katzenpost_status/assets/features/9u-attractors2.js
+++ src/katzenpost_status/assets/features/9u-attractors2.js
@@ -24,4 +24,6 @@
     attractor('chen', 'Chen attractor', function (p, dt) { var a = 35, b = 3, c = 28, x = p[0], y = p[1], z = p[2]; return [x + dt * a * (y - x), y + dt * ((c - a) * x - x * z + c * y), z + dt * (x * y - b * z)]; }, [-0.1, 0.5, -0.6], 0.004, 3600, 0.62, [0, 0, 24], 0x2ec4b6, 70);
 
     attractor('chua', 'Chua attractor', function (p, dt) { var al = 15.6, be = 28, m0 = -1.143, m1 = -0.714, x = p[0], y = p[1], z = p[2]; var f = m1 * x + 0.5 * (m0 - m1) * (Math.abs(x + 1) - Math.abs(x - 1)); return [x + dt * al * (y - x - f), y + dt * (x - y + z), z + dt * (-be * y)]; }, [0.7, 0, 0], 0.02, 4200, 3.4, [0, 0, 0], 0x9b5de5, 60);
+
+    attractor('dadras', 'Dadras attractor', function (p, dt) { var a = 3, b = 2.7, c = 1.7, dd = 2, e = 9, x = p[0], y = p[1], z = p[2]; return [x + dt * (y - a * x + b * y * z), y + dt * (c * y - x * z + z), z + dt * (dd * x * y - e * z)]; }, [1.1, 2.1, -2], 0.01, 4000, 3.0, [0, 0, 0], 0x4d8bf0, 62);
 })();
