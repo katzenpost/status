e8832f9572e0f1662043041bc4103b1c220803d2
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-07 07:27:04 +0200

    Add the Sprott-Linz F attractor view

--- src/katzenpost_status/assets/features/9u-attractors2.js
+++ src/katzenpost_status/assets/features/9u-attractors2.js
@@ -36,4 +36,6 @@
     attractor('sprottb', 'Sprott-B attractor', function (p, dt) { var x = p[0], y = p[1], z = p[2]; return [x + dt * (y * z), y + dt * (x - y), z + dt * (1 - x * y)]; }, [0.5, 0.5, 0.5], 0.02, 4200, 6.5, [0, 0, 0], 0xffd23f, 60);
 
     attractor('lorenz84', 'Lorenz-84 attractor', function (p, dt) { var a = 0.25, b = 4, F = 8, GG = 1, x = p[0], y = p[1], z = p[2]; return [x + dt * (-y * y - z * z - a * x + a * F), y + dt * (x * y - b * x * z - y + GG), z + dt * (b * x * y + x * z - z)]; }, [1, 0, 0], 0.03, 4200, 7.5, [3, 0, 0], 0x2ec4b6, 60);
+
+    attractor('sprottf', 'Sprott-Linz F attractor', function (p, dt) { var a = 0.5, x = p[0], y = p[1], z = p[2]; return [x + dt * (y + z), y + dt * (-x + a * y), z + dt * (x * x - z)]; }, [0.1, 0, 0], 0.02, 4400, 7.0, [0, 0, 1.5], 0x9b5de5, 60);
 })();
