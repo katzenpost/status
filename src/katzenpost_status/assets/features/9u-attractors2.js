e340244582075292e342a53f51b92f4c8beffd66
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-07 06:55:23 +0200

    Add the Four-wing attractor view

--- src/katzenpost_status/assets/features/9u-attractors2.js
+++ src/katzenpost_status/assets/features/9u-attractors2.js
@@ -30,4 +30,6 @@
     attractor('rabinovich', 'Rabinovich-Fabrikant', function (p, dt) { var g = 0.87, a = 1.1, x = p[0], y = p[1], z = p[2]; return [x + dt * (y * (z - 1 + x * x) + g * x), y + dt * (x * (3 * z + 1 - x * x) + g * y), z + dt * (-2 * z * (a + x * y))]; }, [-1, 0, 0.5], 0.004, 4200, 9.0, [0, 0, 0.6], 0xff8f3f, 58);
 
     attractor('nosehoover', 'Nose-Hoover attractor', function (p, dt) { var a = 1.5, x = p[0], y = p[1], z = p[2]; return [x + dt * y, y + dt * (-x + y * z), z + dt * (a - y * y)]; }, [0.1, 0.1, 0], 0.02, 4200, 5.5, [0, 0, 0], 0x00d2a0, 60);
+
+    attractor('fourwing', 'Four-wing attractor', function (p, dt) { var a = 0.2, b = 0.01, c = -0.4, x = p[0], y = p[1], z = p[2]; return [x + dt * (a * x + y * z), y + dt * (b * x + c * y - x * z), z + dt * (-z - x * y)]; }, [1, -1, 1], 0.02, 4400, 6.5, [0, 0, 0], 0xff5d8f, 58);
 })();
