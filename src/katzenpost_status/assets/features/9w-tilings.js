ba76276dae0f27a75baba6d5f10f3332e3681d65
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-08 04:28:55 +0200

    Add the Square tiling view

--- src/katzenpost_status/assets/features/9w-tilings.js
+++ src/katzenpost_status/assets/features/9w-tilings.js
@@ -28,4 +28,6 @@
     tiling('hextiling', 'Hexagonal tiling', function (poly) { for (var i = -R; i <= R; i++) for (var j = -R; j <= R; j++) { var cx = i * 1.5, cy = j * S3 + (i & 1 ? S3 / 2 : 0); poly(ngon(cx, cy, 1, 6, 0)); } }, 0x2ec4b6, 56);
 
     tiling('tritiling', 'Triangular tiling', function (poly) { for (var i = -R; i <= R; i++) for (var j = -R; j <= R; j++) { var x = i + j * 0.5, y = j * S3 / 2; poly([[x, y], [x + 1, y], [x + 0.5, y + S3 / 2]]); poly([[x + 1, y], [x + 1.5, y + S3 / 2], [x + 0.5, y + S3 / 2]]); } }, 0x4d8bf0, 56);
+
+    tiling('sqtiling', 'Square tiling', function (poly) { for (var i = -R; i <= R; i++) for (var j = -R; j <= R; j++) poly([[i, j], [i + 1, j], [i + 1, j + 1], [i, j + 1]]); }, 0x9b5de5, 56);
 })();
