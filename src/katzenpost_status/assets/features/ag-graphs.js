e87cfbe920362ea4ca117bc0a2787ba24720af4f
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-10 11:39:18 +0200

    Add the Complete bipartite K(6,6) view

--- src/katzenpost_status/assets/features/ag-graphs.js
+++ src/katzenpost_status/assets/features/ag-graphs.js
@@ -19,4 +19,6 @@
     build('petersen', 'Petersen graph', function (v, e, T, col) { var O = [], I = [], i; for (i = 0; i < 5; i++) { var th = i / 5 * PI2 - Math.PI / 2; O.push(new T.Vector3(Math.cos(th) * D, Math.sin(th) * D, 0)); I.push(new T.Vector3(Math.cos(th) * D * 0.5, Math.sin(th) * D * 0.5, 0)); v.push(O[i]); v.push(I[i]); } for (i = 0; i < 5; i++) { e.push({ a: O[i], b: O[(i + 1) % 5], color: col }); e.push({ a: I[i], b: I[(i + 2) % 5], color: col }); e.push({ a: O[i], b: I[i], color: col }); } }, 0x2ec4b6, 54);
 
     build('completek12', 'Complete graph K12', function (v, e, T, col) { var P = [], i, j, n = 12; for (i = 0; i < n; i++) { var th = i / n * PI2; P.push(new T.Vector3(Math.cos(th) * D, Math.sin(th) * D, 0)); v.push(P[i]); } for (i = 0; i < n; i++) for (j = i + 1; j < n; j++) e.push({ a: P[i], b: P[j], color: col }); }, 0x4d8bf0, 56);
+
+    build('bipartite', 'Complete bipartite K(6,6)', function (v, e, T, col) { var A = [], B = [], i, j, n = 6; for (i = 0; i < n; i++) { A.push(new T.Vector3(-D * 0.7, (i - (n - 1) / 2) * 6, 0)); B.push(new T.Vector3(D * 0.7, (i - (n - 1) / 2) * 6, 0)); v.push(A[i]); v.push(B[i]); } for (i = 0; i < n; i++) for (j = 0; j < n; j++) e.push({ a: A[i], b: B[j], color: col }); }, 0x9b5de5, 56);
 })();
