3010893f273f48117f8ac60d1bd3f31979c875f1
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-10 20:37:50 +0200

    Add the Geodesic dome (freq 3) view

--- src/katzenpost_status/assets/features/aj-polyzoo.js
+++ src/katzenpost_status/assets/features/aj-polyzoo.js
@@ -16,4 +16,6 @@
     ae('gyrobipyramid', 'Gyroelongated bipyramid', function (a, e, T, color) { var n = 5, R = 13, h = 6, top = ngon(h, R, n, 0), bot = ngon(-h, R, n, Math.PI / n); ring(a, e, top, color); ring(a, e, bot, color); var apT = new T.Vector3(0, h + 12, 0), apB = new T.Vector3(0, -h - 12, 0); a.push(apT); a.push(apB); for (var i = 0; i < n; i++) { e.push({ a: top[i], b: bot[i], color: color }); e.push({ a: top[i], b: bot[(i + n - 1) % n], color: color }); e.push({ a: apT, b: top[i], color: color }); e.push({ a: apB, b: bot[i], color: color }); } }, 0x9b5de5, 54);
 
     ae('elongpyramid', 'Elongated square pyramid (J8)', function (a, e, T, color) { var R = 11, top = ngon(6, R, 4, Math.PI / 4), bot = ngon(-6, R, 4, Math.PI / 4); ring(a, e, top, color); ring(a, e, bot, color); var ap = new T.Vector3(0, 18, 0); a.push(ap); for (var i = 0; i < 4; i++) { e.push({ a: top[i], b: bot[i], color: color }); e.push({ a: ap, b: top[i], color: color }); } }, 0xff8f3f, 54);
+
+    G.create({ id: 'geodesicdome3', name: 'Geodesic dome (freq 3)', rotateSpeed: 0.45, camZ: 54, layout: function (d, T) { var g = new THREE.IcosahedronGeometry(18, 3), pe = fromGeo(g, 0x00d2a0); g.dispose(); return G.anchorLayout(d, T, pe.a, pe.e); } });
 })();
