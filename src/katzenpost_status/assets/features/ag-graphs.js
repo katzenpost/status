1918ce6628fcf1bb315f7357baf9dbfbafe81e61
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-10 12:21:33 +0200

    Add the Pappus graph view

--- src/katzenpost_status/assets/features/ag-graphs.js
+++ src/katzenpost_status/assets/features/ag-graphs.js
@@ -27,4 +27,6 @@
     lcf('heawood', 'Heawood graph', [5, -5], 7, 0xff5d8f);
 
     lcf('mobiuskantor', 'Mobius-Kantor graph', [5, -5], 8, 0x00d2a0);
+
+    lcf('pappus', 'Pappus graph', [5, 7, -7, 7, -7, -5], 3, 0xffd23f);
 })();
