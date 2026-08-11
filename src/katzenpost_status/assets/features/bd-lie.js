cd359d457d1fa743528aa06d2ec89ac25c8ce97d
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-11 14:40:11 +0200

    Add the D5 root system view

--- src/katzenpost_status/assets/features/bd-lie.js
+++ src/katzenpost_status/assets/features/bd-lie.js
@@ -28,4 +28,6 @@
     rootView('c3root', 'C3 root system', function () { return Cn(3); }, 0x00d2a0, 54);
 
     rootView('d4root', 'D4 root system', function () { return Dn(4); }, 0xffd23f, 54);
+
+    rootView('d5root', 'D5 root system', function () { return Dn(5); }, 0x4d8bf0, 54);
 })();
