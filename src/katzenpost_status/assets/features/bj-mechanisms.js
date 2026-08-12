88a66b4a96ea2e2c754387dd05c1e338ccf5f24c
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-12 08:10:51 +0200

    Add the Nephroid caustic view

--- src/katzenpost_status/assets/features/bj-mechanisms.js
+++ src/katzenpost_status/assets/features/bj-mechanisms.js
@@ -7,4 +7,6 @@
     function caustic(id, name, k, color, camZ) { G.create({ id: id, name: name, rotateSpeed: 0.3, camZ: camZ || 54, layout: function (d, THREE) { var N = 200, R = 20, a = [], e = [], i; function pt(x) { var th = (x % N) / N * PI2; return new THREE.Vector3(Math.cos(th) * R, Math.sin(th) * R, 0); } for (i = 0; i < N; i++) { var p = pt(i), q = pt(i * k); a.push(p); e.push({ a: p, b: q, color: color }); } return G.anchorLayout(d, THREE, a, e); } }); }
 
     caustic('cardioidcaustic', 'Cardioid caustic', 2, 0x2ec4b6, 54);
+
+    caustic('nephroidcaustic', 'Nephroid caustic', 3, 0x4d8bf0, 54);
 })();
