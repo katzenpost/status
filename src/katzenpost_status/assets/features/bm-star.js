4b58d7bacb52d3c014f307a5f1302ed9a3ce022d
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-14 23:53:22 +0200

    Add the Compound of five cubes view

--- src/katzenpost_status/assets/features/bm-star.js
+++ src/katzenpost_status/assets/features/bm-star.js
@@ -46,4 +46,6 @@
     compound('dodecaicosacompound', 'Dodecahedron + icosahedron', [{ geo: DOD, rot: [0, 0, 0], color: 0x9b5de5 }, { geo: ICO, rot: [0, 0, 0], color: 0x00d2a0 }], 0x9b5de5, 54);
 
     compound('compound5tetra', 'Compound of five tetrahedra', [0, 1, 2, 3, 4].map(function (k) { return { geo: TET, rot: [0.6, k * 1.2566, 0.3 * k], color: [0x2ec4b6, 0x4d8bf0, 0x9b5de5, 0xff8f3f, 0xff5d8f][k] }; }), 0x2ec4b6, 56);
+
+    compound('compound5cubes', 'Compound of five cubes', [0, 1, 2, 3, 4].map(function (k) { return { geo: B, rot: [0.3, k * 1.2566, 0], color: [0x2ec4b6, 0x4d8bf0, 0x9b5de5, 0xff8f3f, 0xffd23f][k] }; }), 0x2ec4b6, 58);
 })();
