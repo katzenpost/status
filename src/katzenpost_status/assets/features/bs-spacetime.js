c70970e9f8e234e02173878a312b5efaabd7f0d0
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-15 17:50:26 +0200

    Add the Flamm paraboloid view

--- src/katzenpost_status/assets/features/bs-spacetime.js
+++ src/katzenpost_status/assets/features/bs-spacetime.js
@@ -14,4 +14,6 @@
     ae('penrosediagram', 'Penrose diagram', function (a, e, T, color) { var R = 20, N = 8, i; for (i = 0; i <= N; i++) { var t = i / N; e.push({ a: new T.Vector3((t - 0.5) * 2 * R, -R, 0), b: new T.Vector3(0, -R + t * 2 * R, 0), color: color }); } var d = [[-R, 0], [0, R], [R, 0], [0, -R]]; for (i = 0; i < 4; i++) { e.push({ a: new T.Vector3(d[i][0], d[i][1], 0), b: new T.Vector3(d[(i + 1) % 4][0], d[(i + 1) % 4][1], 0), color: 0xffd23f }); a.push(new T.Vector3(d[i][0], d[i][1], 0)); } for (i = 1; i < N; i++) { var s = i / N; e.push({ a: new T.Vector3(-R + s * R, s * R - R + R, 0), b: new T.Vector3(R - s * R, s * R, 0), color: color }); } }, 0x9b5de5, 56);
 
     surf('blackholefunnel', 'Black-hole funnel', function (u, v, T) { var r = 2 + u * 22, th = v * PI2, z = -30 / Math.sqrt(r); return new T.Vector3(r * Math.cos(th), z, r * Math.sin(th)); }, 40, 50, 0x4d8bf0, 58);
+
+    surf('flammparaboloid', 'Flamm paraboloid', function (u, v, T) { var M = 2, r = 2 * M + u * 22, th = v * PI2, z = 2 * Math.sqrt(2 * M * (r - 2 * M)); return new T.Vector3(r * Math.cos(th), z - 12, r * Math.sin(th)); }, 40, 50, 0x9b5de5, 58);
 })();
