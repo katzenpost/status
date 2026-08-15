df07164bc6bcf028b87aa042aed7fd6a02a0f933
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-15 15:27:53 +0200

    Add the Pyrochlore lattice view

--- src/katzenpost_status/assets/features/br-honeycomb.js
+++ src/katzenpost_status/assets/features/br-honeycomb.js
@@ -16,4 +16,6 @@
     lattice('hcplattice', 'HCP lattice', function () { var V = [], i, j, k, S3 = Math.sqrt(3); for (k = -1; k <= 1; k++) for (i = -2; i <= 2; i++) for (j = -2; j <= 2; j++) { var off = (k & 1) ? 0.5 : 0; V.push([(i + off + (j % 2 ? 0.5 : 0)) * S, j * S3 / 2 * S / 1 * 0.5 * 1.0, k * S * 0.82]); } return V; }, 0xff8f3f, 58);
 
     lattice('octettruss', 'Octet truss (FCC)', function () { var V = [], x, y, z; for (x = -2; x <= 2; x++) for (y = -2; y <= 2; y++) for (z = -2; z <= 2; z++) if (((x + y + z) & 1) === 0 && x * x + y * y + z * z <= 8) V.push([x * S * 0.7, y * S * 0.7, z * S * 0.7]); return V; }, 0xff5d8f, 58);
+
+    lattice('pyrochlore', 'Pyrochlore lattice', function () { var V = [], seen = {}, x, y, z; var basis = [[0, 0, 0], [0.25, 0.25, 0], [0.25, 0, 0.25], [0, 0.25, 0.25]]; for (x = -1; x <= 1; x++) for (y = -1; y <= 1; y++) for (z = -1; z <= 1; z++) basis.forEach(function (b) { V.push([(x + b[0]) * S * 1.6, (y + b[1]) * S * 1.6, (z + b[2]) * S * 1.6]); }); return V; }, 0x00d2a0, 58);
 })();
