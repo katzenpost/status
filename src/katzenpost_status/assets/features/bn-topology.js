d6c28d201975e1168376782357dc96ae9bbe1dc3
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-15 03:14:00 +0200

    Add the Supershape (superformula) view

--- src/katzenpost_status/assets/features/bn-topology.js
+++ src/katzenpost_status/assets/features/bn-topology.js
@@ -27,4 +27,6 @@
     surf('spindletorus', 'Spindle torus', function (u, v, T) { var a = u * PI2, b = v * PI2, R = 6, r = 12; return new T.Vector3((R + r * Math.cos(b)) * Math.cos(a), r * Math.sin(b), (R + r * Math.cos(b)) * Math.sin(a)); }, 60, 44, 0x4d8bf0, 58);
 
     surf('supertoroid', 'Supertoroid', function (u, v, T) { var a = u * PI2 - Math.PI, b = v * PI2 - Math.PI, R = 12, r = 5, e1 = 0.4, e2 = 0.4; function sp(x, e) { var c = Math.cos(x); var s = Math.sin(x); return [Math.sign(c) * Math.pow(Math.abs(c), e), Math.sign(s) * Math.pow(Math.abs(s), e)]; } var A2 = sp(a, e2), B2 = sp(b, e1); return new T.Vector3((R + r * B2[0]) * A2[0], r * B2[1], (R + r * B2[0]) * A2[1]); }, 50, 50, 0x9b5de5, 58);
+
+    surf('supershape', 'Supershape (superformula)', function (u, v, T) { var th = u * PI2 - Math.PI, ph = v * Math.PI - Math.PI / 2; var r1 = sf(7, 0.2, 1.7, 1.7, 1, th), r2 = sf(7, 0.2, 1.7, 1.7, 1, ph), S = 16; return new T.Vector3(S * r1 * Math.cos(th) * r2 * Math.cos(ph), S * r2 * Math.sin(ph), S * r1 * Math.sin(th) * r2 * Math.cos(ph)); }, 60, 50, 0xff8f3f, 56);
 })();
