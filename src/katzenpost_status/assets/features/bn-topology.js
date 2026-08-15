1e3af49a368ec0cdbda12afa2766cb5e9e047709
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-15 03:03:26 +0200

    Add the Supertoroid view

--- src/katzenpost_status/assets/features/bn-topology.js
+++ src/katzenpost_status/assets/features/bn-topology.js
@@ -25,4 +25,6 @@
     surf('horntorus', 'Horn torus', function (u, v, T) { var a = u * PI2, b = v * PI2, R = 10, r = 10; return new T.Vector3((R + r * Math.cos(b)) * Math.cos(a), r * Math.sin(b), (R + r * Math.cos(b)) * Math.sin(a)); }, 60, 40, 0x2ec4b6, 58);
 
     surf('spindletorus', 'Spindle torus', function (u, v, T) { var a = u * PI2, b = v * PI2, R = 6, r = 12; return new T.Vector3((R + r * Math.cos(b)) * Math.cos(a), r * Math.sin(b), (R + r * Math.cos(b)) * Math.sin(a)); }, 60, 44, 0x4d8bf0, 58);
+
+    surf('supertoroid', 'Supertoroid', function (u, v, T) { var a = u * PI2 - Math.PI, b = v * PI2 - Math.PI, R = 12, r = 5, e1 = 0.4, e2 = 0.4; function sp(x, e) { var c = Math.cos(x); var s = Math.sin(x); return [Math.sign(c) * Math.pow(Math.abs(c), e), Math.sign(s) * Math.pow(Math.abs(s), e)]; } var A2 = sp(a, e2), B2 = sp(b, e1); return new T.Vector3((R + r * B2[0]) * A2[0], r * B2[1], (R + r * B2[0]) * A2[1]); }, 50, 50, 0x9b5de5, 58);
 })();
