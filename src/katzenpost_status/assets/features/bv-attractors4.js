e8e8c274dd47f75f7fe4d4ce9fb7c51569344456
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-17 18:14:17 +0200

    Add the Moore-Spiegel attractor view

--- src/katzenpost_status/assets/features/bv-attractors4.js
+++ src/katzenpost_status/assets/features/bv-attractors4.js
@@ -22,4 +22,6 @@
     attractor('shimizumorioka', 'Shimizu-Morioka attractor', function (p, dt) { var a = 0.75, b = 0.45, x = p[0], y = p[1], z = p[2]; return [x + dt * y, y + dt * (x * (1 - z) - a * y), z + dt * (-b * z + x * x)]; }, [0.1, 0.1, 0.1], 0.02, 5000, 13, [0, 0, 1], 0xff8f3f, 58);
 
     attractor('windmi', 'WINDMI attractor', function (p, dt) { var a = 0.7, b = 2.5, x = p[0], y = p[1], z = p[2]; return [x + dt * y, y + dt * z, z + dt * (-a * z - y + b - Math.exp(x))]; }, [0, 0.8, 0], 0.03, 5000, 6, [0, 0, 0], 0xff5d8f, 58);
+
+    attractor('moorespiegel', 'Moore-Spiegel attractor', function (p, dt) { var t = 6, r = 20, x = p[0], y = p[1], z = p[2]; return [x + dt * y, y + dt * z, z + dt * (-z - (t - r + r * x * x) * y - t * x)]; }, [0.1, 0, 0], 0.01, 5000, 4, [0, 0, 0], 0x00d2a0, 58);
 })();
