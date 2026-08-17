3a8215eb1476035f62b1b456271c8b36f3cfff14
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-17 21:56:02 +0200

    Add the Shear flow view

--- src/katzenpost_status/assets/features/bw-fields.js
+++ src/katzenpost_status/assets/features/bw-fields.js
@@ -29,4 +29,6 @@
     field('field-dipole', 'Dipole field', function (x, y, z) { var r = Math.sqrt(x * x + y * y + z * z) + 0.05, r5 = Math.pow(r, 5); return [3 * z * x / r5, 3 * z * y / r5, (3 * z * z - r * r) / r5]; }, 0xff8f3f, 56, 5, 14, 70, 0.12);
 
     field('field-abc', 'ABC flow', function (x, y, z) { var A = 1, B = 0.7, C = 0.43, X = x * 3, Y = y * 3, Z = z * 3; return [A * Math.sin(Z) + C * Math.cos(Y), B * Math.sin(X) + A * Math.cos(Z), C * Math.sin(Y) + B * Math.cos(X)]; }, 0xff5d8f, 58, 5, 16, 90, 0.12);
+
+    field('field-shear', 'Shear flow', function (x, y, z) { return [y, 0.15 * z, -0.15 * y]; }, 0x00d2a0, 58, 5, 16, 50, 0.16);
 })();
