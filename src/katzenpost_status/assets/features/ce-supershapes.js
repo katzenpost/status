ba45f40002fc05c7e2858649b3382eb002d9b3e3
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-21 10:35:08 +0200

    Add the Supershape star view

---
+++ src/katzenpost_status/assets/features/ce-supershapes.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+    var K = window.KATZEN;
+    if (!K || !window.KATZEN_GEO3D) return;
+    var G = window.KATZEN_GEO3D, PI = Math.PI;
+    function fit(A, R) { var i, n = A.length, cx = 0, cy = 0, cz = 0; for (i = 0; i < n; i++) { cx += A[i].x; cy += A[i].y; cz += A[i].z; } cx /= n; cy /= n; cz /= n; var mx = 1e-9; for (i = 0; i < n; i++) { A[i].x -= cx; A[i].y -= cy; A[i].z -= cz; mx = Math.max(mx, Math.abs(A[i].x), Math.abs(A[i].y), Math.abs(A[i].z)); } var s = R / mx; for (i = 0; i < n; i++) { A[i].x *= s; A[i].y *= s; A[i].z *= s; } return A; }
+    function gridEdges(A, idx, U, Vn, color, wrapU, wrapV) { var E = [], i, j; function add(a, b, c, e) { var k1 = idx[a + '_' + b], k2 = idx[c + '_' + e]; if (k1 != null && k2 != null) E.push({ a: A[k1], b: A[k2], color: color }); } for (i = 0; i < U; i++) for (j = 0; j < Vn; j++) { add(i, j, i + 1, j); add(i, j, i, j + 1); } if (wrapV) for (i = 0; i <= U; i++) add(i, Vn, i, 0); if (wrapU) for (j = 0; j <= Vn; j++) add(U, j, 0, j); return E; }
+    function surf(id, name, pfn, U, Vn, color, camZ, wrapU, wrapV) { G.create({ id: id, name: name, rotateSpeed: 0.34, camZ: camZ || 60, stellate: false, layout: function (d, T) { var A = [], idx = {}, i, j; for (i = 0; i <= U; i++) for (j = 0; j <= Vn; j++) { idx[i + '_' + j] = A.length; A.push(pfn(i / U, j / Vn, T)); } fit(A, 18); return G.anchorLayout(d, T, A, gridEdges(A, idx, U, Vn, color, wrapU, wrapV)); } }); }
+    function superr(theta, m, n1, n2, n3) { var t = m * theta / 4, a = Math.pow(Math.abs(Math.cos(t)), n2), b = Math.pow(Math.abs(Math.sin(t)), n3), r = Math.pow(a + b, -1 / n1); if (!isFinite(r)) r = 0; return Math.min(r, 6); }
+    function ss(P) { return function (u, v, T) { var th = -PI + 2 * PI * u, ph = -PI / 2 + PI * v, r1 = superr(th, P[0], P[1], P[2], P[3]), r2 = superr(ph, P[4], P[5], P[6], P[7]); return new T.Vector3(r1 * Math.cos(th) * r2 * Math.cos(ph), r1 * Math.sin(th) * r2 * Math.cos(ph), r2 * Math.sin(ph)); }; }
+
+    surf('ss-star6', 'Supershape star', ss([6,0.3,1.7,1.7,6,0.3,1.7,1.7]), 48, 24, 0x2ec4b6, 60, true, false);
+})();
