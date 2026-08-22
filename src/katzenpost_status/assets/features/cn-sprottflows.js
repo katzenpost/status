8fa2ac676f6b68cdbba4b87090c9f96ff0855211
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-22 11:24:01 +0200

    Add the Sprott R flow view

--- src/katzenpost_status/assets/features/cn-sprottflows.js
+++ src/katzenpost_status/assets/features/cn-sprottflows.js
@@ -23,4 +23,6 @@
     fl('sprotto', 'Sprott O flow', function (p, h) { return [p[0] + h * p[1], p[1] + h * (p[0] - p[2]), p[2] + h * (p[0] + p[0] * p[2] + 2.7 * p[1])]; }, 0.008, 0xff5d6c);
 
     fl('sprottp', 'Sprott P flow', function (p, h) { return [p[0] + h * (2.7 * p[1] + p[2]), p[1] + h * (-p[0] + p[1] * p[1]), p[2] + h * (p[0] + p[1])]; }, 0.008, 0x33ccff);
+
+    fl('sprottr', 'Sprott R flow', function (p, h) { return [p[0] + h * (0.9 - p[1]), p[1] + h * (0.4 + p[2]), p[2] + h * (p[0] * p[1] - p[2])]; }, 0.02, 0x8a5bff);
 })();
