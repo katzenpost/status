73269bae15cfafad21c09abc44d7ae12d34724f9
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-22 09:59:32 +0200

    Add the Sprott H flow view

--- src/katzenpost_status/assets/features/cn-sprottflows.js
+++ src/katzenpost_status/assets/features/cn-sprottflows.js
@@ -7,4 +7,6 @@
     function fl(id, name, step, dt, color, camZ) { G.create({ id: id, name: name, rotateSpeed: 0.4, camZ: camZ || 62, stellate: false, layout: function (d, T) { var p = [0.05, 0.05, 0.05], i, P = []; for (i = 0; i < 1500; i++) p = step(p, dt); for (i = 0; i < 5000; i++) { p = step(p, dt); if (!isFinite(p[0]) || !isFinite(p[1]) || !isFinite(p[2]) || Math.abs(p[0]) > 1e3) break; P.push(new T.Vector3(p[0], p[1], p[2])); } fit(P, 18); return G.curveLayout(d, T, P, color); } }); }
 
     fl('sprotta', 'Sprott A flow', function (p, h) { return [p[0] + h * p[1], p[1] + h * (-p[0] + p[1] * p[2]), p[2] + h * (1 - p[1] * p[1])]; }, 0.02, 0x2ec4b6);
+
+    fl('sprotth', 'Sprott H flow', function (p, h) { return [p[0] + h * (-p[1] + p[2] * p[2]), p[1] + h * (p[0] + 0.5 * p[1]), p[2] + h * (p[0] - p[2])]; }, 0.008, 0x4d8bf0);
 })();
