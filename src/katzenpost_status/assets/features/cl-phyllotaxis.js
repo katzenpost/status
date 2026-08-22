093b016eaa7fc3d88b7d349850e3c6c5b6ce98c4
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-22 05:19:43 +0200

    Add the Phyllotaxis cone view

--- src/katzenpost_status/assets/features/cl-phyllotaxis.js
+++ src/katzenpost_status/assets/features/cl-phyllotaxis.js
@@ -11,4 +11,6 @@
     phy('phy-daisy', 'Phyllotaxis daisy', 900, 0.8, function (r) { return 0; }, 0x4d8bf0);
 
     phy('phy-dome', 'Phyllotaxis dome', 700, 0.9, function (r) { return 8 - r * r * 0.02; }, 0x9b5de5);
+
+    phy('phy-cone', 'Phyllotaxis cone', 700, 0.9, function (r) { return r * 0.6; }, 0xff5d8f);
 })();
