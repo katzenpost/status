932c74351858760abfe6cc76c808a4ca9651c758
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-25 16:34:20 +0200

    Add the Newton basins (z^5-1) view

--- src/katzenpost_status/assets/features/cy-newtonfractals.js
+++ src/katzenpost_status/assets/features/cy-newtonfractals.js
@@ -78,4 +78,6 @@
     basins('nb-newton-quartic', 'Newton basins (z^4-1)', polyPow(4), stepNewton, 1, 1.7, 56);
 
     basins('nb-halley-quartic', 'Halley basins (z^4-1)', polyPow(4), stepHalley, 1, 1.7, 56);
+
+    basins('nb-newton-quintic', 'Newton basins (z^5-1)', polyPow(5), stepNewton, 1, 1.7, 56);
 })();
