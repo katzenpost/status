b6aed04c7777ca1dfa71370f012fc5aa28e4b0f2
Jacob Appelbaum <jacob@appelbaum.net>
2026-08-27 15:07:19 +0200

    Add the Hopf fibres (hex base) view

--- src/katzenpost_status/assets/features/df-polytopes4d.js
+++ src/katzenpost_status/assets/features/df-polytopes4d.js
@@ -70,4 +70,6 @@
     hopf('p4-hopf-linked', 'Hopf fibres (linking)', [[0.4, 0], [0.7, 1.4], [1.0, 2.8], [1.2, 4.2], [0.55, 5.4]], 60);
 
     hopf('p4-hopf-spiral', 'Hopf fibres over a spiral', (function () { var b = [], j; for (j = 0; j < 28; j++) b.push([0.25 + 1.1 * j / 28, 2 * Math.PI * j * 3 / 28]); return b; })(), 60);
+
+    hopf('p4-hopf-hexbase', 'Hopf fibres (hex base)', (function () { var b = [[0.5, 0]], i; for (i = 0; i < 6; i++) b.push([1.0, 2 * Math.PI * i / 6]); for (i = 0; i < 6; i++) b.push([0.75, 2 * Math.PI * (i + 0.5) / 6]); return b; })(), 60);
 })();
